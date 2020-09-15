8fb37d6ccc1432541d92353f0b2cf2b9569801d6
mroberts <mroberts@isystematics.com>
2020-09-15 12:59:02 -0400

    First git for twitter django project

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,7 @@
+#!/Users/michael/PycharmProjects/django_learning/venv/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.2','django-admin.py'
+__requires__ = 'Django==3.2'
+__import__('pkg_resources').require('Django==3.2')
+__file__ = '/Users/michael/PycharmProjects/django_learning/django/django/bin/django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
