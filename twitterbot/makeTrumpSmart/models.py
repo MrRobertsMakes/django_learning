4cd38d09635e4085f9d7f0cbad9c40ee548a5302
mroberts <mroberts@isystematics.com>
2020-09-16 16:55:39 -0400

    create model

--- twitterbot/makeTrumpSmart/models.py
+++ twitterbot/makeTrumpSmart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TargetTwitter(models.Model):
+    handle = models.CharField(max_length=200)
+    tweet_number = models.IntegerField(default=0)
--- twitterbot/makeTrumpSmart/tasks.py
+++ twitterbot/makeTrumpSmart/tasks.py
@@ -2,12 +2,12 @@
 from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
-from demoapp.models import Widget
+# from demoapp.models import Widget
 
 
 @shared_task
 def add(x, y):
     return x + y
 
-# ToDo add task to import tweets from trump
+# ToDo add task to import latest tweet from trump
 # ToDo add task to run the tweet through paraphrasing web tool
