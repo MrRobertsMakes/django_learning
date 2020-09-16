ac28c660f101322cda4ddc3a31796809c03f1ae8
mroberts <mroberts@isystematics.com>
2020-09-16 15:54:49 -0400

    update allowed hosts

---
+++ twitterbot/makeTrumpSmart/tasks.py
@@ -0,0 +1,13 @@
+# Create your tasks here
+from __future__ import absolute_import, unicode_literals
+
+from celery import shared_task
+from demoapp.models import Widget
+
+
+@shared_task
+def add(x, y):
+    return x + y
+
+# ToDo add task to import tweets from trump
+# ToDo add task to run the tweet through paraphrasing web tool
